f05d44764788f223289a96afa1e0e6da93a80244
Venkatachalam <harisanthosh94@gmail.com>
2020-02-20 12:21:27 +0100

    Chrome server hosting files

---
+++ index.js
@@ -0,0 +1,33 @@
+
+//import NeoVis from '/node_modules/neovis.js/dist/neovis.js';
+
+
+var viz;
+
+    function draw() {
+        var config = {
+            container_id: "viz",
+            server_url: "bolt://localhost:7687",
+            server_user: "neo4j",
+            server_password: "honeywell123!",
+            labels: {
+                "Assembly1": {
+                    "caption": "name",
+                    "size": "pagerank",
+                    "community": "community"
+                }
+            },
+            relationships: {
+                "MOVES_ITEM": {
+                    "thickness": "weight",
+                    "caption": false
+                }
+            },
+            initial_cypher: "MATCH (n)-[r:MOVES_ITEM]->(m) RETURN *"
+        };
+
+        viz = new NeoVis.default(config);
+        viz.render();
+    }
+	
+	draw();
