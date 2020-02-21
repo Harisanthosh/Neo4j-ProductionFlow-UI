41e853134b3da71d3e50bf2ccb65d0e93cd2eaf6
Venkatachalam <harisanthosh94@gmail.com>
2020-02-21 16:10:48 +0100

    Styling the neo4j viewer

--- index.js
+++ index.js
@@ -30,5 +30,29 @@ var viz;
         viz = new NeoVis.default(config);
         viz.render();
     }
-	setInterval(draw, 3000);
-	// draw();
+
+    function customdraw() {
+        var config = {
+            container_id: "viz",
+            server_url: "bolt://localhost:7687",
+            server_user: "neo4j",
+            server_password: "honeywell123!",
+            labels: {
+                "Gaszähler": {
+                    "caption": "name"
+                }
+            },
+            relationships: {
+                "GEHÖRT_ZU": {
+                    "thickness": "weight",
+                    "caption": false
+                }
+            },
+            initial_cypher: "MATCH (n)-[r:GEHÖRT_ZU]->(m) RETURN *"
+        };
+
+        viz = new NeoVis.default(config);
+        viz.render();
+    }
+	// setInterval(customdraw, 5000);
+	customdraw();
