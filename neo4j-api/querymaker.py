1255de7eb0e0a9404258ff15e3ad35fae65f9640
Venkatachalam <harisanthosh94@gmail.com>
2020-02-21 10:22:44 +0100

    Changes made to decipher relationship between workcenters

--- neo4j-api/querymaker.py
+++ neo4j-api/querymaker.py
@@ -18,21 +18,53 @@ def runpg():
         if len(val) < 6:
             wc_leader_arr.append(1)
         else:
-            wc_leader_arr.append(0)
+            if (len(label_extr[key+1]) > len(val)):
+                wc_leader_arr.append(1)
+            else:
+                wc_leader_arr.append(0)
 
     file['Leader'] = wc_leader_arr
     neo4j_map['Leader'] = wc_leader_arr
-    print(file.head())
+    # print(file.head())
+    parent_label = []
+    childe_label = []
+    current_label = ""
 
     for key,val in enumerate(label_extr):
         if key == 0:
             cypher_creater += f'CREATE (:Gaszähler {{name: {val}, processname: {neo4j_map["Name"][key][key]}}}),'
+            parent_label.append(val)
+            childe_label.append(label_extr[key+1])
+            current_label = val
         elif (key == len(label_extr)-1):
-            cypher_creater += f' (:Gaszähler {{name: {val}, processname: {neo4j_map["Name"][0][key]}}}))'
+            cypher_creater += f' (:Gaszähler {{name: {val}, processname: {neo4j_map["Name"][0][key]}}})'
+            # For last node the realtionship will be pointed to itself even though it's a leader WC
+            if neo4j_map['Leader'][key] == 1:
+                parent_label.append(val)
+                childe_label.append(val)
+            elif neo4j_map['Leader'][key] == 0:
+                parent_label.append(val)
+                childe_label.append(val)
+
         else:
-            cypher_creater += f' (:Gaszähler {{name: {val}, processname: {neo4j_map["Name"][0][key]}}})),'
+            cypher_creater += f' (:Gaszähler {{name: {val}, processname: {neo4j_map["Name"][0][key]}}}),'
+            if ((neo4j_map['Leader'][key] == 1) and (len(val) <= len(label_extr[key+1]))):
+                parent_label.append(val)
+                childe_label.append(label_extr[key+1])
+                current_label = val
+            elif ((neo4j_map['Leader'][key] == 1) and (len(val) >= len(label_extr[key+1]))):
+                parent_label.append(val)
+                childe_label.append(val)
+                current_label = val
+            elif neo4j_map['Leader'][key] == 0:
+                parent_label.append(current_label)
+                childe_label.append(val)
 
     print(cypher_creater)
+    file['Parent'] = parent_label
+    file['Child'] = childe_label
+    print(file.head())
+    file.to_csv('updated_wc_sap.csv',encoding='utf-8-sig',sep=';',index=False)
 
 if __name__ == "__main__":
     runpg()
