000b935069196e23bb475d900ab7783ffe939c5c
Venkatachalam <harisanthosh94@gmail.com>
2020-02-27 16:53:10 +0100

    Control simulation via cmdline

--- simpy-models/PMFormatter.py
+++ simpy-models/PMFormatter.py
@@ -1,6 +1,7 @@
 import simpy
 from datetime import datetime
 import pandas as pd
+import sys
 
 prom_formatter = {}
 prom_formatter['caseId'] = []
@@ -10,7 +11,7 @@ prom_formatter['Activity'] = []
 env = simpy.RealtimeEnvironment()
 
 def speaker(env, speaker):
-    yield env.timeout(3)
+    yield env.timeout(int(sys.argv[2]))
     valx = f'Speaker {speaker} finishes his speech at {env.now} interval on time {datetime.now()}'
     prom_formatter['caseId'].append(speaker)
     prom_formatter['Timestamp'].append(datetime.now())
@@ -32,8 +33,10 @@ def moderator(env):
 
         print(val)
 
-env.process(moderator(env))
-env.run()
+if __name__ == "__main__":
+    env.process(moderator(env))
+    print(f'the time taken for each speaker is {sys.argv[1]} at an interval {sys.argv[2]}')
+    env.run()
 
 
 
