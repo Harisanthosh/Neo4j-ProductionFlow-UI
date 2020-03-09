a1634f1e4cc8265211fe4797f6772eb7bea21836
Venkatachalam <harisanthosh94@gmail.com>
2020-03-09 16:07:16 +0100

    Bug fixing for simulation

--- simpy-models/A10206Simulator.py
+++ simpy-models/A10206Simulator.py
@@ -9,6 +9,7 @@ import string
 global cols_list
 global resource_list
 global wc_list
+global ops_list
 global processing_time
 global waiting_time
 
@@ -17,7 +18,6 @@ def sfc_generator(size=10, chars=string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def startSimulator(df2):
-    print(df2.head())
     #Using iloc to access can also use iat or at to access elements as matrix
     sfc_sim = str(df2.iloc[0]['SFC'])[:6]
     sfc_act = int(sfc_sim)
@@ -25,7 +25,17 @@ def startSimulator(df2):
     print(sfc_act)
     print(sfc_gen)
     merged_sfc = str(sfc_act) + str(sfc_gen)
-    print(merged_sfc)
+    print(f'Starting simulation for the SFC {merged_sfc}')
+    df_construct = {}
+    new_df = pd.DataFrame(columns=cols_list)
+    #print(new_df)
+    for index,row in df2.iterrows():
+        #print(index, row['OPERATION'],row['PROCESSING_TIME_SECS'],row['WAITING_TIME_SECS'])
+        row['SFC'] = merged_sfc
+        curr_row = pd.DataFrame([row],columns=cols_list)
+        # print(curr_row)
+        new_df = new_df.append([curr_row],ignore_index=True)
+    print(new_df.head())
 
 
 
@@ -38,16 +48,16 @@ if __name__ == "__main__":
     global wc_list
     global processing_time
     global waiting_time
+    global ops_list
     cols_list = df1.columns
     resource_list = df1['RESRCE']
     wc_list = df1['WORK_CENTER']
     processing_time = df1['PROCESSING_TIME_SECS']
     waiting_time = df1['WAITING_TIME_SECS']
+    ops_list = df1['OPERATION']
     print(cols_list)
     print(len(cols_list))
     #for key,val in df1.iterrows():
-
-
 
     startSimulator(df1)
     #print(resource_list)
