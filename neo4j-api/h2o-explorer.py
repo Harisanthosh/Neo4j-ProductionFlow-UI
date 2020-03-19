b50f6027a1f55c546b17e8ae877943b9c13526df
Venkatachalam <harisanthosh94@gmail.com>
2020-03-19 12:42:07 +0100

    Infer from saved H2o MOJO model using Random forest regression

--- neo4j-api/h2o-explorer.py
+++ neo4j-api/h2o-explorer.py
@@ -5,13 +5,13 @@ model_path = 'C:/Users/H395978/AppData/Local/Programs/Thesis/stamm/ml_models/h20
 #saved_model = h2o.load_model(model_path)
 #Make sure h2o.init() is executed in a separate terminal
 h2o.connect()
-cols = ['Sum(WAITING_TIME_SECS)','Unique concatenate(SHOP_ORDER_BO)','Mean(QTY_DONE)','Unique count(WORK_CENTER)','Unique count*(RESRCE)','Unique concatenate*(ITEM)','Last(SFC_DONE)','Mean(QTY_RELEASED)','Mean(QTY_ORDERED)','Unique count(SHOP_ORDER_BO)','Unique count*(ITEM)','Mean(QTY)']
-vals = ['13886.768','ShopOrderBO:0001,5238406','0.03','10','21','80000988','true','2376','2376','1','1','1']
+cols = ['Sum(WAITING_TIME_SECS)','Unique concatenate(SHOP_ORDER_BO)','Mean(QTY_DONE)','Unique count(OPERATION)','Unique count(WORK_CENTER)','Unique count*(RESRCE)','Unique concatenate*(ITEM)','Last(SFC_DONE)','Mean(QTY_RELEASED)','Mean(QTY_ORDERED)','Unique count(SHOP_ORDER_BO)','Unique count*(ITEM)','Mean(QTY)']
+vals = ['13886.768','ShopOrderBO:0001,5238406','0.03','27','10','21','80000988','true','2376','2376','1','1','1']
 df = pd.DataFrame([vals],columns=cols)
 hf = h2o.H2OFrame(df)
 #Importing to work further, for making single shot predictions it is not needed
 mojo_model = h2o.import_mojo(model_path)
-print(mojo_model)
+#print(mojo_model)
 predict = mojo_model.predict(hf)
 print(predict)
 #mojo_model.varimp_plot()
