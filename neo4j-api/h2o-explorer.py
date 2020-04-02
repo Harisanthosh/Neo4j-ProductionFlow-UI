be9ab03bfa1b2c79050ea381176dbdcf4f837800
Venkatachalam <harisanthosh94@gmail.com>
2020-04-02 14:51:26 +0200

    Added Tkinter UI for validating the predicted results

--- neo4j-api/h2o-explorer.py
+++ neo4j-api/h2o-explorer.py
@@ -1,6 +1,9 @@
 import h2o
 import pandas as pd
 from datetime import datetime, timedelta
+import tkinter as tk
+
+
 
 model_path = 'C:/Users/H395978/AppData/Local/Programs/Thesis/stamm/ml_models/h20_regression_trees'
 #saved_model = h2o.load_model(model_path)
@@ -10,17 +13,42 @@ cols = ['Sum(WAITING_TIME_SECS)','Unique concatenate(SHOP_ORDER_BO)','Mean(QTY_D
 vals = ['13886.768','ShopOrderBO:0001,5238406','0.03','27','10','21','80000988','true','2376','2376','1','1','1']
 df = pd.DataFrame([vals],columns=cols)
 hf = h2o.H2OFrame(df)
-#Importing to work further, for making single shot predictions it is not needed
-mojo_model = h2o.import_mojo(model_path)
-#print(mojo_model)
-predict = mojo_model.predict(hf)
-orig_time = f"{datetime.now():%d-%m-%Y %H:%M:%S}"
-print(predict)
-predicted_val = datetime.now() + timedelta(seconds=int(predict))
-predicted_time = f"{predicted_val:%d-%m-%Y %H:%M:%S}"
-print(orig_time)
-print('The estimated time of completion for the SFC is')
-print(predicted_time)
+#num1 = int(input('Enter the number of SFCs to release:'))
+global num1
+def show_entry_fields():
+    global num1
+    num1 = e2.get()
+    print(f"Number of SFC's are {num1}")
+    #Importing to work further, for making single shot predictions it is not needed
+    mojo_model = h2o.import_mojo(model_path)
+    #print(mojo_model)
+    predict = mojo_model.predict(hf)
+    orig_time = f"{datetime.now():%d-%m-%Y %H:%M:%S}"
+    print(predict)
+    predicted_val = datetime.now() + timedelta(seconds=int(predict))
+    predicted_time = f"{predicted_val:%d-%m-%Y %H:%M:%S}"
+    print(f'The Start time for the SFC is {orig_time}')
+    print(f'The estimated time of completion for the first SFC is {predicted_time}')
+    full_time = predict * num1
+    full_val = datetime.now() + timedelta(seconds=int(full_time))
+    shoporder_time = f"{full_val:%d-%m-%Y %H:%M:%S}"
+    print(f'The estimated time of completion for the last SFC for the Shop Order is {shoporder_time}')
+master = tk.Tk()
+# tk.Label(master,
+#          text="First Name").grid(row=0)
+tk.Label(master,
+         text="Number of SFC's to Release").grid(row=1)
+
+e2 = tk.Entry(master)
+
+e2.grid(row=1, column=1)
+tk.Button(master,
+          text='Show', command=show_entry_fields).grid(row=3,
+                                                       column=1,
+                                                       sticky=tk.W,
+                                                       pady=4)
+
+tk.mainloop()
 #mojo_model.varimp_plot()
 #h2o.print_mojo(model_path, format='json', tree_index=None)
 
